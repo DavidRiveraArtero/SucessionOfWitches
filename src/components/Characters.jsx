e49ec9d84c4b139f5ca47bc339781a4d757322e1
David Rivera <davidtoy222@gmail.com>
2024-01-30 11:46:36 +0100

    Carousel Character

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,22 +1,41 @@
-import pj from '../../public/json/characters.json'
+import pj from '/src/json/characters.json'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// IMPORT SWIPER STYLE
+import "swiper/css";
+import "./character.css";
 
 export function Characters(){
     return(
       <>
-        
+      <Swiper>
+
         {
             pj.personajes.map((personaje,index) => {
-                return (
-                    <>
-                        <h1 className="relative text-4xl z-10">{personaje.name}</h1>
-                        <img className=" object-cover z-0 absolute scale-x-[-1] h-[520px]" src={personaje.img} alt={personaje.name} />
-                    </>
-                
+                return(
+                    <SwiperSlide key={index} >
+                        <div className='flex flex-col'>
+                            <h1 className="text-5xl z-10 text-center">{personaje.name}</h1>
+                            {
+                                personaje.name == "Gojo" ? 
+                                <img className=" w-[50%] object-cover drop-shadow-md absolute z-0 scale-x-[-1] 2xl:h-[627px] rounded-lg sm:h-[355px]" 
+                                    src={personaje.img} 
+                                    alt={personaje.name}/> 
+                                    :     
+                                <img className=" object-cover drop-shadow-md absolute z-0 scale-x-[-1] 2xl:h-[627px] rounded-lg sm:h-[355px]" 
+                                    src={personaje.img} 
+                                    alt={personaje.name}/>
+                            }
+                        
+                        </div>
+                    </SwiperSlide>
                 )
             })
         }
+      </Swiper>
       
       
       </>
+      
     )
 }
